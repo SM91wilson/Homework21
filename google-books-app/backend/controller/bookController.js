43bcd025ec72caad591ad8ca61a72043bdf02951
tango <sm91wilson@gmail.com>
2020-07-19 14:21:33 -0400

    done book controller

--- google-books-app/backend/controller/bookController.js
+++ google-books-app/backend/controller/bookController.js
@@ -1,1 +1,25 @@
-const 
+const Book = require('../model/books');
+
+module.exports={
+    findAll: function (req, res){
+        Book.find(req.search).then(dbBook => res.json(dbBook))
+            .catch(error => res.json(error));
+
+    },
+    findById: function (req, res){
+        Book.findById(req.params.id).then(dbBook => res.json(dbBook))
+            .catch(error => res.json(error));
+
+    },
+    create: function (req, res){
+        Book.create(req.body).then(dbBook => res.json(dbBook))
+            .catch(error => res.json(error));
+
+    },
+    remove: function (req, res){
+        Book.findById(req.params.id).then(dbBook => dbBook.remove())
+            .then(dbBook => res.json(dbBook))
+            .catch(error => res.json(error));
+
+    }
+};

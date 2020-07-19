94797ebc438af031eeed8043d5556057e3e3c051
tango <sm91wilson@gmail.com>
2020-07-18 21:56:07 -0400

    adding to server

--- google-books-app/backend/server.js
+++ google-books-app/backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, function(){
+    console.log(`connected on ${PORT}`);
+});
